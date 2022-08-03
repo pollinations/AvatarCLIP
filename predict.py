daab873628fb1d671b45d68a1c6144cffb9b34bd
Ubuntu <ubuntu@ip-172-31-4-151.ec2.internal>
2022-08-03 09:43:47 +0000

    fix model paths

--- predict.py
+++ predict.py
@@ -2,12 +2,14 @@ from cog import BasePredictor, Path, Input
 import os  
 from glob import glob
 
-MODEL_PATHS = "--smpl_model_folder /smpl_data --AE_path_fname /avatarclip_data/model_VAE_16.pth --codebook_fname /avatarclip_data/codebook.pth"
+#MODEL_PATHS = "--smpl_model_folder /smpl_data --AE_path_fname /avatarclip_data/model_VAE_16.pth --codebook_fname /avatarclip_data/codebook.pth"
 
 class Predictor(BasePredictor):
     def setup(self):
         """Run pip install git+https://github.com/voodoohop/neural_renderer.git"""
         os.system('pip install git+https://github.com/voodoohop/neural_renderer.git')
+        os.system('mv /avatarclip_data /src/AvatarGen/ShapeGen/data')
+        os.system('mv /smpl_data /src/smpl_models')
 
     def predict(self,
             text: str = Input(description="Coarse character prompt", default="overweight sumo wrestler")
@@ -15,7 +17,7 @@ class Predictor(BasePredictor):
         """Run python main.py --target_txt '[text]' in folder ./AvatarGen/ShapeGen"""
         previouspath = os.getcwd()
         os.chdir("./AvatarGen/ShapeGen/")
-        os.system(f'python main.py {MODEL_PATHS} --target_txt "{text}"')
+        os.system(f'python main.py --target_txt "{text}"')
         filepath = glob("./output/coarse_shape/*.obj")[0]
         print("returning",filepath)
         return Path(filepath)
